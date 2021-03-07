78afce62e91a1ceaf703bc59b836c65b810d088a
Mustafa <mouss42490@gmail.com>
2021-03-07 14:10:36 +0100

    working on GAE (google app engine)

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
+# [START gae_python38_render_template]
+# [START gae_python3_render_template]
+import datetime
+
 from flask import Flask, render_template, request
 import pymongo
 import os 
 import json
 
+
 print(os.getcwd())
-
-
 # MONGODB_URL = os.environ['MONGODB_URL']
 # MONGODB_NAME = os.environ['MONGODB_NAME']
 
-MONGODB_URL='127.0.0.1:27019'
-MONGODB_NAME='mydb'
+# MONGODB_URL='127.0.0.1:27019'
+# MONGODB_NAME='mydb'
 
-client = pymongo.MongoClient(MONGODB_URL)
-collections = client[MONGODB_NAME]
+# client = pymongo.MongoClient(MONGODB_URL)
+# collections = client[MONGODB_NAME]
 
-
-def create_app():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def homepage():
-        return render_template('index.html')
-
-    @app.route('/scripts_in_templates.js')
-    def script():
-        return render_template('scripts_in_templates.js', color='pink')
-
-    @app.route('/trying/', methods=['GET','POST'])
-    def trying():
-        if request.method == "POST":
-            kwargs = json.loads(request.form.get('data'))
-            print(kwargs)
-            return {"success": True}
+app = Flask(__name__)
 
 
-    return app
+@app.route('/')
+def root():
+    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
+                   datetime.datetime(2018, 1, 2, 10, 30, 0),
+                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   ]
 
+    return render_template('index.html', times=dummy_times)
+    
+# @app.route('/myscripts.js')
+# def script():
+#     return render_template('myscripts.js', color='pink')
+
+@app.route('/trying/', methods=['GET','POST'])
+def trying():
+    if request.method == "POST":
+        kwargs = json.loads(request.form.get('data'))
+        print(kwargs)
+        return {"success": True}
+
+
+# return app
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
@@ -46,6 +52,6 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    create_app().run(host='127.0.0.1', port=8080, debug=True)
-
-
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python3_render_template]
+# [END gae_python38_render_template]
---
+++ static/script.js
@@ -0,0 +1,12 @@
+
+// [START gae_python38_log]
+// [START gae_python3_log]
+'use strict';
+
+window.addEventListener('load', function () {
+
+  console.log("Hello World!");
+
+});
+// [END gae_python3_log]
+// [END gae_python38_log]
