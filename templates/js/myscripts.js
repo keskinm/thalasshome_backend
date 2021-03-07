1eb1adb652b3b1d31e35dbd0ce881a523fe160c0
Mustafa <mouss42490@gmail.com>
2021-03-07 17:42:43 +0100

    try add cross domain

--- templates/js/myscripts.js
+++ templates/js/myscripts.js
@@ -7,6 +7,7 @@ function validate() {
 
     $.ajax({  
         type : 'POST',
+        crossDomain: true,
         // url : "{{url_for('trying')}}",
         // url : "https://locations-ecommerce.ew.r.appspot.com:8080/posting_scripts/trying",
         url : "https://locations-ecommerce.ew.r.appspot.com/posting_scripts/trying",
