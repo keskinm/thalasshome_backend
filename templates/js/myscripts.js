1ea5bde0d0ac136d20118ac99dc0aca3919b344f
Mustafa <mouss42490@gmail.com>
2021-03-07 17:29:28 +0100

    no weird errors

--- templates/js/myscripts.js
+++ templates/js/myscripts.js
@@ -8,8 +8,8 @@ function validate() {
     $.ajax({  
         type : 'POST',
         // url : "{{url_for('trying')}}",
-        url : "https://locations-ecommerce.ew.r.appspot.com/posting_scripts/trying",
-        // url : "https://locations-ecommerce.ew.r.appspot.com:8080/posting_scripts/trying",
+        url : "https://locations-ecommerce.ew.r.appspot.com:8080/posting_scripts/trying",
+        // url : "https://locations-ecommerce.ew.r.appspot.com/posting_scripts/trying",
         data : {"data": JSON.stringify(value)}, 
         success:function(result){
             console.log(result);
