6812a4ee4f662cc1596ad9885447706a067ad8bc
Mustafa <mouss42490@gmail.com>
2021-03-09 12:04:09 +0100

    change name

--- get_orders.py
+++ get_orders.py
@@ -3,9 +3,9 @@ from google.cloud import datastore
 
 datastore_client = datastore.Client()
 
-def print_order():
+def print_hardcode_updated_order():
     key = datastore_client.key("Orders", "harcoded_id_01")
     task = datastore_client.get(key)
     print("order", task)
 
-print_quantity()
+print_hardcode_updated_order()
