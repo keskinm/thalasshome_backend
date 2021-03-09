b9f55e80122b4263cf3669da1f5c8b4a78624e36
Mustafa <mouss42490@gmail.com>
2021-03-09 12:00:32 +0100

    some changes

--- get_quantity.py
+++ get_quantity.py
@@ -3,9 +3,9 @@ from google.cloud import datastore
 
 datastore_client = datastore.Client()
 
-def print_quantity():
-    key = datastore_client.key("Task", "sampletask1")
+def print_order():
+    key = datastore_client.key("Orders", "harcoded_id_01")
     task = datastore_client.get(key)
-    print("quantity:", task)
+    print("order", task)
 
 print_quantity()
--- main.py
+++ main.py
@@ -13,26 +13,15 @@ from google.cloud import datastore
 datastore_client = datastore.Client()
 
 
-def create_and_save_quantity():
-    kind = "Task"
-    name = "sampletask1"
+def create_and_save_order_test():
+    kind = "Orders"
+    name = "harcoded_id_01"
     task_key = datastore_client.key(kind, name)
     task = datastore.Entity(key=task_key)
-    task["quantity"] = 0
+    task["employee"] = 'mario'
     datastore_client.put(task)
-# create_and_save_quantity()
+create_and_save_order_test()
 
-
-def update_quantity():
-    key = datastore_client.key("Task", "sampletask1")
-    task = datastore_client.get(key)
-    task['quantity'] = task['quantity'] + 1
-    datastore_client.put(task)
-
-def print_quantity():
-    key = datastore_client.key("Task", "sampletask1")
-    task = datastore_client.get(key)
-    print("quantity:", task)
 
 print(os.getcwd())
 
@@ -78,13 +67,6 @@ def trying():
 # return app
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='0.0.0.0', port=8080, debug=True)
 # [END gae_python3_render_template]
 # [END gae_python38_render_template]
