4fc5a92f47b67bf1f317629a1004ec6563dca2b7
Mustafa <mouss42490@gmail.com>
2021-03-10 18:33:46 +0100

    Add maps

---
+++ utils/maps/maps.py
@@ -0,0 +1,15 @@
+
+
+zip_codes_to_locations = {
+    'ile_de_france': ['75000', '77000', '78000', '95000', '94000', '93000', '92000', '91000'],
+    'loire': ['42000', '69000']
+}
+
+employees_to_location = {
+
+    'loire': ['mustafa', 'romain', 'elyes'],
+    'ile_de_france': ['mehdi'],
+}
+
+
+
