865cf80de4e30dc7ddc8fc97563e986511ecdba0
Mustafa <mouss42490@gmail.com>
2021-03-14 18:28:14 +0100

    Fix

--- starters/daemons/start_webserver.sh
+++ starters/daemons/start_webserver.sh
@@ -1,1 +1,2 @@
+cd ../..&&\
 gunicorn -b 0.0.0.0:8000 dashboard.main:app --daemon
--- starters/daemons/start_websocket.sh
+++ starters/daemons/start_websocket.sh
@@ -1,1 +1,2 @@
+cd ../..&&\
 gunicorn -b 0.0.0.0:1337 websocket.ws:app --worker-class sanic.worker.GunicornWorker --daemon
