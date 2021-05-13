fc089d0dbd4a54c38642d7689fecac91502398b1
Mustafa <mouss42490@gmail.com>
2021-05-13 19:06:45 +0200

    Add some todos

---
+++ todos.py
@@ -0,0 +1,11 @@
+"""
+
+- display balances of split payments
+- correct refresh issues (+ add a refresh button?)
+- make system work only with compute engine (with https for hook and bcz its better) or only with app engine (with ws calls working..)
+- improve design
+- add sms notifications
+- refactor db attributes and add user parametrabilisation
+
+"""
+
