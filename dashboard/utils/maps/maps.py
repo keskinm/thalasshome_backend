708ee35c39fb0025e0622300bdd23e2069bee529
Mustafa <mouss42490@gmail.com>
2021-10-02 19:17:49 +0200

    Small refactoring on zones

--- dashboard/utils/maps/maps.py
+++ dashboard/utils/maps/maps.py
@@ -1,11 +1,13 @@
 zip_codes_to_locations = {
     'France': {
         'ile_de_france': ['75', '77', '78', '95', '94', '93', '92', '91'],
-        'loire': ['42', '69'],
+        'loire': ['42'],
         'pays_de_gex': ['01'],
         'toulouse': ['31'],
         'centre_val_de_loire': ['18', '28', '36', '37', '41', '45'],
         'var': ['83'],
+        'rhone': ['69'],
+        'marseille': ['13']
     },
 
     'Switzerland': {
