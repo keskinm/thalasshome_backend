280cd0779dd3ccf9dbd66ecf0d7928d4e40f0189
Mustafa <mouss42490@gmail.com>
2021-10-03 12:54:24 +0200

    Some refactoring on signup

--- dashboard/db/queries.py
+++ dashboard/db/queries.py
@@ -7,12 +7,12 @@ class Queries:
     def __init__(self, handling_class=User):
         self.engine = create_engine('sqlite:///providers.db', echo=True)
         self.session = sessionmaker(bind=self.engine)()
-        self.handing_class = handling_class
+        self.handling_class = handling_class
 
     def unique(self, column_name):
         providers_table = self.session.query(User).filter()
         zones = list(map(lambda x: getattr(x, column_name), list(providers_table)))
-        zones = list(dict.fromkeys(list(zones)))
+        zones = list(dict.fromkeys(zones))
         return zones
 
     def aggregate_by_column(self, column_name, selection=None):
@@ -20,7 +20,7 @@ class Queries:
 
         aggregated = {}
         for item in unique_column:
-            item_aggregated_list = list(self.session.query(User).filter(getattr(self.handing_class, column_name) == item))
+            item_aggregated_list = list(self.session.query(User).filter(getattr(self.handling_class, column_name) == item))
             if selection:
                 item_aggregated_list = list(map(lambda x: getattr(x, selection), item_aggregated_list))
             aggregated.update({item: item_aggregated_list})
@@ -32,7 +32,6 @@ class Queries:
         user = db_session.query(User).filter()
         d = (list(user))
         d
-
 
 # agg = Queries().aggregate_by_column('zone')
 # print(agg)
